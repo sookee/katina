7f97af61105904c587a354b280e836ec7a48895d
sookee <oasookee@gmail.com>
2013-01-02 13:09:11 +0000

    added VERSION constants

--- src/oastats.cpp
+++ src/oastats.cpp
@@ -5,10 +5,32 @@
  *      Author: oasookee@gmail.com
  */
 
+#include <iostream>
 
+#include "types.h"
+
+using namespace oastats::types;
+
+#ifndef STAMP
+#define STAMP
+#endif
+#ifndef REVISION
+#define REVISION
+#endif
+#ifndef COMMITS
+#define COMMITS
+#endif
+#ifndef DEV
+#define DEV
+#endif
+
+const str VERSION = STAMP "-" REVISION "-" COMMITS DEV;
+//-D DEV=\"$(shell git diff --quiet || echo -dev)\" \
+//-D COMMITS=\"$(shell printf %04d \"$(git log --after={yesterday} --pretty=oneline|wc -l)\")\" \
+//-D REVISION=\"$(shell git log -n 1 --pretty=format:%h|tr [:lower:] [:upper:])\"
 
 
 int main()
 {
-
+	std::cout << VERSION << '\n';
 }
