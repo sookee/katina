25fcfcf5de3559b9144a01c1690998e3d3164909
sookee <oasookee@gmail.com>
2013-01-02 14:16:48 +0000

    Added headers to deps in Makefile Added codes.h

--- src/oastats.cpp
+++ src/oastats.cpp
@@ -5,7 +5,11 @@
  *      Author: oasookee@gmail.com
  */
 
+#include <fstream>
+#include <sstream>
 #include <iostream>
+#include <string>
+#include <initializer_list>
 
 #include "types.h"
 
@@ -26,7 +30,56 @@ using namespace oastats::types;
 
 const str VERSION = STAMP "-" REVISION "-" COMMITS DEV;
 
-int main()
+struct LineProcessor
 {
-	std::cout << VERSION << '\n';
+	virtual bool operator()(const str& line) = 0;
+};
+
+class KillsLineProcessor
+: public LineProcessor
+{
+private:
+	str_map k;
+	str_map d;
+
+public:
+	bool operator()(const str& line)
+	{
+
+	}
+};
+
+int main(int argc, char* argv[])
+{
+	std::cout << "oastats: " << VERSION << '\n';
+
+	std::ifstream ifs;
+	std::ofstream ofs;
+
+	if(argc > 1)
+	{
+		ifs.open(argv[1]);
+		if(!ifs)
+		{
+			std::cerr << "Error opening input file: " << argv[1] << '\n';
+			std::exit(1);
+		}
+	}
+
+	if(argc > 2)
+	{
+		ofs.open(argv[2]);
+		if(!ofs)
+		{
+			std::cerr << "Error opening ouput file: " << argv[2] << '\n';
+			std::exit(2);
+		}
+	}
+
+	std::istream& is = argc > 1 ? ifs : std::cin;
+	std::ostream& os = argc > 2 ? ofs : std::cout;
+
+	str line;
+	while(std::getline(is, line))
+		;//process(line);
 }
