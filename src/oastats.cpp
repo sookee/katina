918160900a4a5d21495b77616f2dd44d9cdc31aa
sookee <oasookee@gmail.com>
2013-01-02 13:23:24 +0000

    Added mysql & pcre libs to Makefile

--- src/oastats.cpp
+++ src/oastats.cpp
@@ -25,10 +25,6 @@ using namespace oastats::types;
 #endif
 
 const str VERSION = STAMP "-" REVISION "-" COMMITS DEV;
-//-D DEV=\"$(shell git diff --quiet || echo -dev)\" \
-//-D COMMITS=\"$(shell printf %04d \"$(git log --after={yesterday} --pretty=oneline|wc -l)\")\" \
-//-D REVISION=\"$(shell git log -n 1 --pretty=format:%h|tr [:lower:] [:upper:])\"
-
 
 int main()
 {
