5509e53c6958be6304299e1c381623f1bb2bbeef
sookee <oasookee@gmail.com>
2013-05-04 20:50:17 +0100

    Finished PKI, untested

--- src/PKI.cpp
+++ src/PKI.cpp
@@ -21,156 +21,184 @@ using namespace oastats::types;
 using namespace oastats::utils;
 
 PKI::PKI()
+: skey(0)
+, pkey(0)
 {
 	if(!gcry_check_version(GCRYPT_VERSION))
 	{
-		log("PKI: ERROR version check failed: " << GCRYPT_VERSION);
+		log("PKI: ERROR: version check failed: " << GCRYPT_VERSION);
 	}
 	gcry_control(GCRYCTL_INITIALIZATION_FINISHED, 0);
 }
 
 PKI::~PKI()
 {
+	gcry_sexp_release(skey);
+	gcry_sexp_release(pkey);
+	for(key_map_iter i = pkeys.begin(); i != pkeys.end(); ++i)
+		gcry_sexp_release(i->second);
 }
 
-bool PKI::generate_keys()
+bool PKI::generate_keypair(siz bits)
 {
-	str keydata;
-	return generate_keys(keydata);
-}
+	gcry_sexp_t params = 0;
 
-bool PKI::generate_keys(str& keydata)
-{
-	siz errof;
-	gcry_sexp_t params;
-	if(gcry_error_t e = gcry_sexp_build(&params, &errof, "(genkey (rsa (nbits 3:256)))"))
+	soss oss;
+	oss << bits;
+	str sbits = oss.str();
+
+	oss.str("");
+	oss << "(genkey (rsa (nbits " << sbits.size() << ":" << sbits << ")))";
+
+	con("genkey param: " << oss.str() << "(" << bits << ")");
+
+	if(gcry_error_t e = gcry_sexp_build(&params, 0, oss.str().c_str()))
 	{
-		log("PKI: ERROR setting parameters: " << gcry_strerror(e));
+		log("PKI: ERROR: setting parameters: " << gcry_strerror(e));
+		log("PKI:      : " << oss.str());
 		return false;
 	}
 
-	gcry_sexp_t keys;
-	if(gcry_error_t e = gcry_pk_genkey(&keys, params))
+	gcry_sexp_t keypair;
+	if(gcry_error_t e = gcry_pk_genkey(&keypair, params))
 	{
-		log("PKI: ERROR generating keys: " << gcry_strerror(e));
+		gcry_sexp_release(params);
+		log("PKI: ERROR: generating keys: " << gcry_strerror(e));
+		log("PKI:      : " << oss.str());
 		return false;
 	}
 
-	if(!(skey = gcry_sexp_find_token(keys, "private-key", 0)))
+	// DEBUG
+	str s;
+	if(get_sexp_as_text(keypair, s))
+		con("XXX: " << s);
+
+	if(!set_keys(keypair))
 	{
-		log("PKI: ERROR retrieving private key");
+		gcry_sexp_release(keypair);
 		return false;
 	}
 
-	if(!(pkey = gcry_sexp_find_token(keys, "public-key", 0)))
-	{
-		log("PKI: ERROR retrieving public key");
-		return false;
-	}
+	gcry_sexp_release(keypair);
 
 	return true;
 }
 
-bool PKI::set_skey(const str& s)
+bool PKI::set_keys(const gcry_sexp_t keypair)
 {
-	siz errof = 0;
-	if(gcry_error_t e = gcry_sexp_build(&skey, &errof, s.c_str()))
+	gcry_sexp_t skey = 0;
+
+	if(!(skey = gcry_sexp_find_token(keypair, "private-key", 0)))
 	{
-		log("PKI: ERROR creating private key: " << gcry_strerror(e));
-		log("PKI: " << s);
+		log("PKI: ERROR: retrieving private key");
 		return false;
 	}
+
+	gcry_sexp_t pkey = 0;
+
+	if(!(pkey = gcry_sexp_find_token(keypair, "public-key", 0)))
+	{
+		gcry_sexp_release(skey);
+		log("PKI: ERROR: retrieving public key");
+		return false;
+	}
+
+	gcry_sexp_release(this->skey);
+	this->skey = skey;
+
+	gcry_sexp_release(this->pkey);
+	this->pkey = pkey;
+
 	return true;
 }
 
-bool PKI::set_pkey(const str& s)
-{
-	siz errof = 0;
-	if(gcry_error_t e = gcry_sexp_build(&pkey, &errof, s.c_str()))
-	{
-		log("PKI: ERROR creating public key: " << gcry_strerror(e));
-		log("PKI: " << s);
-		return false;
-	}
-	return true;
-}
-
-bool PKI::load_keys(const str& file)
+bool PKI::load_keypair(const str& file)
 {
 	std::ifstream ifs(file.c_str());
 	if(!ifs.is_open())
 	{
-		log("PKI: Error opening file; " << file);
+		log("PKI: ERROR: opening file; " << file);
 		return false;
 	}
-	return read_keys(ifs);
+	return read_keypair(ifs);
 }
 
-bool PKI::read_keys(std::istream& is)
+bool PKI::read_keypair(std::istream& is)
 {
-	str line;
-	str_vec parts;
-	while(std::getline(is, line))
-		parts.push_back(line);
-
-	if(parts.size() != 6)
+	soss oss;
+	if(!(oss << is.rdbuf()))
 	{
-		log("PKI: ERROR: reading keys");
+		log("PKI: ERROR: reading keypair from stream");
+		return false;
+	}
+	return read_keypair(oss.str());
+}
+
+bool PKI::read_keypair(const str& s)
+{
+	gcry_sexp_t keypair = 0;
+
+	if(gcry_error_t e = gcry_sexp_sscan(&keypair, 0, s.c_str(), s.size()))
+	{
+		log("PKI: ERROR: parsing keypair data: " << gcry_strerror(e));
+		log("PKI: " << s);
 		return false;
 	}
 
-	soss oss;
-	oss << "(private-key (rsa ";
-	oss << "(n #" << parts[0] << "#)";
-	oss << "(e #" << parts[1] << "#)";
-	oss << "(d #" << parts[2] << "#)";
-	oss << "(p #" << parts[3] << "#)";
-	oss << "(q #" << parts[4] << "#)";
-	oss << "(u #" << parts[5] << "#)";
-	oss << "))";
-
-	if(!set_skey(oss.str().c_str()))
+	if(!set_keys(keypair))
+	{
+		gcry_sexp_release(keypair);
 		return false;
+	}
 
-	oss.str("");
-
-	oss << "(public-key (rsa ";
-	oss << "(n #" << parts[0] << "#)";
-	oss << "(e #" << parts[1] << "#)";
-	oss << "))";
-
-	if(!set_pkey(oss.str().c_str()))
-		return false;
+	gcry_sexp_release(keypair);
 
 	return true;
 }
 
-bool PKI::encrypt(const str& text, str& code)
+bool PKI::load_public_key(const str& id, const str& file)
 {
-	gcry_sexp_t data;
-
-	// convert text into data
-
-	siz errof = 0;
-	if(gcry_error_t e = gcry_sexp_build(&data, &errof, "%s", text.c_str()))
+	std::ifstream ifs(file.c_str());
+	if(!ifs.is_open())
 	{
-		log("PKI: ERROR: converting text to sexp: " << gcry_strerror(e));
+		log("PKI: ERROR: opening file; " << file);
+		return false;
+	}
+	return read_public_key(id, ifs);
+}
+
+bool PKI::read_public_key(const str& id, std::istream& is)
+{
+	soss oss;
+	if(!(oss << is.rdbuf()))
+	{
+		log("PKI: ERROR: reading keypair from stream");
+		return false;
+	}
+	return read_public_key(id, oss.str());
+}
+
+bool PKI::read_public_key(const str& id, const str& s)
+{
+	gcry_sexp_t pkey = 0;
+
+	if(gcry_error_t e = gcry_sexp_sscan(&pkey, 0, s.c_str(), s.size()))
+	{
+		log("PKI: ERROR: parsing public key data: " << gcry_strerror(e));
+		log("PKI: " << s);
 		return false;
 	}
 
-	gcry_sexp_t result;
+	pkeys[id] = pkey;
 
-	if(gcry_error_t e = gcry_pk_encrypt(&result, data, pkey))
-	{
-		log("PKI: encrypt error: " << gcry_strerror(e));
-		return false;
-	}
+	return true;
+}
 
-	//con("DUMP: " << __func__);
-	gcry_sexp_dump(result);
-
+bool PKI::get_sexp_as_text(const gcry_sexp_t& sexp, str& text)
+{
 	siz len = 0;
-	char* buff = new char[(len = gcry_sexp_sprint(result, GCRYSEXP_FMT_CANON, 0, 0))];
+
+	char* buff = new char[(len = gcry_sexp_sprint(sexp, GCRYSEXP_FMT_ADVANCED, 0, 0))];
 
 	if(!buff)
 	{
@@ -178,71 +206,237 @@ bool PKI::encrypt(const str& text, str& code)
 		return false;
 	}
 
-	if(!(len = gcry_sexp_sprint(result, GCRYSEXP_FMT_CANON, buff, len)))
+	if(!(len = gcry_sexp_sprint(sexp, GCRYSEXP_FMT_ADVANCED, buff, len)))
 	{
 		log("PKI: ERROR: converting sexp to string");
 		delete[] buff;
 		return false;
 	}
 
-	code.assign(buff, len);
+	text.assign(buff, len);
 	delete[] buff;
+
+	return true;
+}
+
+bool PKI::get_keypair_as_text(str& keypair)
+{
+	gcry_sexp_t keys = 0;
+	if(gcry_error_t e = gcry_sexp_build (&keys, 0, "(key-data\n  %S\n  %S\n )", pkey, skey))
+	{
+		log("PKI: ERROR: concatenating keys: " << gcry_strerror(e));
+		return false;
+	}
+
+	if(!get_sexp_as_text(keys, keypair))
+	{
+		gcry_sexp_release(keys);
+		return false;
+	}
+
+	gcry_sexp_release(keys);
+//	key_t key = gcry_sexp_append(pkey, skey);
+
+	return true;
+}
+
+bool PKI::get_public_key_as_text(str& text)
+{
+	return get_sexp_as_text(pkey, text);
+}
+
+bool PKI::get_private_key_as_text(str& text)
+{
+	return get_sexp_as_text(skey, text);
+}
+
+bool PKI::encrypt(const str& id, const str& text, str& code)
+{
+	gcry_sexp_t data = 0;
+
+	if(pkeys.find(id) == pkeys.end())
+	{
+		log("PKI: ERROR: Unknown id: " << id);
+		return false;
+	}
+
+	if(gcry_error_t e = gcry_sexp_build(&data, 0, "%s", text.c_str()))
+	{
+		log("PKI: ERROR: converting text to sexp: " << gcry_strerror(e));
+		return false;
+	}
+
+	gcry_sexp_t result = 0;
+
+	if(gcry_error_t e = gcry_pk_encrypt(&result, data, pkeys.at(id)))
+	{
+		gcry_sexp_release(data);
+		log("PKI: encrypt error: " << gcry_strerror(e));
+		return false;
+	}
+
+	gcry_sexp_release(data);
+
+	if(!get_sexp_as_text(result, code))
+	{
+		gcry_sexp_release(result);
+		return false;
+	}
+
+	gcry_sexp_release(result);
 
 	return true;
 }
 
 bool PKI::decrypt(const str& code, str& text)
 {
-	gcry_sexp_t data;
+	gcry_sexp_t data = 0;
 
 	// convert code into data
 
-	siz errof = 0;
-//	if(gcry_error_t e = gcry_sexp_build(&data, &errof, "%s", code.c_str()))
-	if(gcry_error_t e = gcry_sexp_new(&data, code.c_str(), code.size(), 0))
+	if(gcry_error_t e = gcry_sexp_new(&data, code.c_str(), code.size(), 1))
 	{
 		log("PKI: ERROR: converting code to sexp: " << gcry_strerror(e));
 		return false;
 	}
 
-	gcry_sexp_t result;
+	gcry_sexp_t result = 0;
 
 	if(gcry_error_t e = gcry_pk_decrypt(&result, data, skey))
 	{
+		gcry_sexp_release(data);
 		log("PKI: decrypt error: " << gcry_strerror(e));
 		return false;
 	}
 
-	//con("DUMP: " << __func__);
-	gcry_sexp_dump(result);
+	gcry_sexp_release(data);
 
-	siz len = 0;
-	char* buff = new char[(len = gcry_sexp_sprint(result, GCRYSEXP_FMT_DEFAULT, 0, 0))];
-
-	if(!buff)
+	if(!get_sexp_as_text(result, text))
 	{
-		log("PKI: ERROR: creating buffer");
+		gcry_sexp_release(result);
 		return false;
 	}
 
-	if(!(len = gcry_sexp_sprint(result, GCRYSEXP_FMT_DEFAULT, buff, len)))
+	text.erase(0, text.find_first_not_of("\n\" "));
+	text.erase(text.find_last_not_of("\n\" ") + 1);
+
+	gcry_sexp_release(result);
+
+	return true;
+}
+
+bool PKI::create_sexp_from_text(const str& exp, gcry_sexp_t& sexp)
+{
+	gcry_sexp_t data = 0;
+
+	if(gcry_error_t e = gcry_sexp_build(&data, 0, "%s", exp.c_str()))
 	{
-		log("PKI: ERROR: converting sexp to string");
-		delete[] buff;
+		log("PKI: ERROR: converting text to sexp: " << gcry_strerror(e));
 		return false;
 	}
 
-	str line;
-	siss iss(str(buff, len));
-	if(!std::getline(std::getline(iss, line, ':'), line))
+	sexp = data;
+
+	return true;
+}
+
+bool PKI::create_signature(str& signature)
+{
+	return create_signature(pkey, signature);
+}
+
+bool PKI::create_signature(const str& text, str& signature)
+{
+	gcry_sexp_t data = 0;
+
+	if(!create_sexp_from_text(text, data))
+		return false;
+
+	if(!create_signature(data, signature))
 	{
-		log("PKI: ERROR: extracting text from sexp: " << str(buff, len));
-		delete[] buff;
+		gcry_sexp_release(data);
 		return false;
 	}
 
-	text = line;
-	delete[] buff;
+	gcry_sexp_release(data);
+
+	return true;
+}
+
+bool PKI::create_signature(const gcry_sexp_t& sexp, str& signature)
+{
+	gcry_sexp_t result = 0;
+
+	if(gcry_error_t e = gcry_pk_sign(&result, sexp, skey))
+	{
+		log("PKI: signing error: " << gcry_strerror(e));
+		return false;
+	}
+
+	if(!get_sexp_as_text(result, signature))
+	{
+		gcry_sexp_release(result);
+		return false;
+	}
+
+	gcry_sexp_release(result);
+
+	return true;
+}
+
+bool PKI::verify_signature(const str& id, const str& signature, bool& is_good)
+{
+	if(pkeys.find(id) == pkeys.end())
+	{
+		log("PKI: ERROR: Unknown id: " << id);
+		return false;
+	}
+
+	gcry_sexp_t sigval = 0;
+
+	if(gcry_error_t e = gcry_sexp_sscan(&sigval, 0, signature.c_str(), signature.size()))
+	{
+		log("PKI: ERROR: parsing signature data: " << gcry_strerror(e));
+		log("PKI: " << signature);
+		return false;
+	}
+
+	is_good = gcry_pk_verify(sigval, pkeys.at(id), pkeys.at(id)) == 0;
+
+	gcry_sexp_release(sigval);
+
+	return true;
+}
+
+bool PKI::verify_signature(const str& id, const str& signature, const str& text, bool& is_good)
+{
+	if(pkeys.find(id) == pkeys.end())
+	{
+		log("PKI: ERROR: Unknown id: " << id);
+		return false;
+	}
+
+	gcry_sexp_t sigval = 0;
+
+	if(gcry_error_t e = gcry_sexp_sscan(&sigval, 0, signature.c_str(), signature.size()))
+	{
+		log("PKI: ERROR: parsing signature data: " << gcry_strerror(e));
+		log("PKI: " << signature);
+		return false;
+	}
+
+	gcry_sexp_t data = 0;
+
+	if(!create_sexp_from_text(text, data))
+	{
+		gcry_sexp_release(sigval);
+		return false;
+	}
+
+	is_good = gcry_pk_verify(sigval, data, pkeys.at(id)) == 0;
+
+	gcry_sexp_release(sigval);
+	gcry_sexp_release(data);
 
 	return true;
 }
--- src/include/katina/PKI.h
+++ src/include/katina/PKI.h
@@ -14,33 +14,87 @@ namespace oastats { namespace pki {
 
 class PKI
 {
-	gcry_sexp_t skey;
-	gcry_sexp_t pkey;
+public:
+	typedef gcry_sexp_t key_t;
+	typedef std::map<str, key_t> key_map;
+	typedef std::pair<const str, key_t> key_map_pair;
+	typedef key_map::iterator key_map_iter;
+	typedef key_map::const_iterator key_map_citer;
+
+private:
+	key_t skey;
+	key_t pkey;
+
+	std::map<str, gcry_sexp_t> pkeys;
+
+	bool set_keys(const key_t keypair);
+	bool create_sexp_from_text(const str& exp, gcry_sexp_t& sexp);
+	bool get_sexp_as_text(const key_t& sexp, str& text);
+	bool create_signature(const gcry_sexp_t& sexp, str& signature);
 
 public:
 	PKI();
 	virtual ~PKI();
 
-	bool generate_keys();
-	bool generate_keys(str& keydata);
-
-	bool set_skey(const str& s);
-	bool set_pkey(const str& s);
-	bool load_keys(const str& file);
-	bool read_keys(std::istream& is);
+	bool generate_keypair(siz bits = 512);
 
 	/**
-	 * Encrypt text using public key
+	 * Add a public key for encrypting data to its owner.
+     * @param id A means to identify the public key's owner
+     * @param s public key in sexp form
+     * @return true on success else false
+     */
+	bool read_public_key(const str& id, const str& s);
+
+	/**
+	 * Add a public key for encrypting data to its owner.
+     * @param id A means to identify the public key's owner
+     * @param is input must contain the public key in sexp form
+     * @return true on success else false
+     */
+	bool read_public_key(const str& id, std::istream& is);
+
+	/**
+	 * Add a public key for encrypting data to its owner.
+	 * @param id A means to identify the public key's owner
+     * @param file name of a file that must contain the public key in sexp form
+     * @return true on success else false
+     */
+	bool load_public_key(const str& id, const str& file);
+
+	/**
+	 * Set the keypair
+     * @param s
+     * @return
+     */
+	bool read_keypair(const str& s);
+	bool read_keypair(std::istream& is);
+	bool load_keypair(const str& file);
+
+	bool get_keypair_as_text(str& keypair);
+	bool get_public_key_as_text(str& text);
+	bool get_private_key_as_text(str& text);
+
+	/**
+	 * Encrypt text using public key belonging to id
+	 * @param id The public key owner's id
+	 * @param text The clear-text message to encrypt
+	 * @param code Either the encrypted version of text or
+	 * unchanged on error.
+	 * @return true = success else false
 	 */
-	bool encrypt(const str& text, str& code);
+	bool encrypt(const str& id, const str& text, str& code);
 
 	/**
 	 * Decrypt text using private key
 	 */
 	bool decrypt(const str& code, str& text);
 
-private:
+	bool create_signature(str& signature); // use pkey
+	bool create_signature(const str& text, str& signature);
 
+	bool verify_signature(const str& id, const str& signature, bool& is_good); // pkey
+	bool verify_signature(const str& id, const str& signature, const str& text, bool& is_good);
 };
 
 }} oastats::pki
