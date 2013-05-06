de2dcb42f1a0eadfe63fa7529c2193ea4e67c7ff
sookee <oasookee@gmail.com>
2013-05-06 17:41:56 +0100

    Add: Winner Stays On plugin

---
+++ src/plugins/WinnerStaysOn.cpp
@@ -0,0 +1,109 @@
+
+#include "WinnerStaysOn.h"
+
+#include <katina/types.h>
+#include <katina/log.h>
+
+namespace katina { namespace plugin {
+
+using namespace oastats::log;
+using namespace oastats::types;
+
+KATINA_PLUGIN_TYPE(WinnerStaysOn);
+KATINA_PLUGIN_INFO("katina::winner-stays-on", "Winner Stays On", "0.1-dev");
+
+bool WinnerStaysOn::open()
+{
+	return true;
+}
+
+str WinnerStaysOn::get_id() const
+{
+	return ID;
+}
+
+str WinnerStaysOn::get_name() const
+{
+	return NAME;
+}
+
+str WinnerStaysOn::get_version() const
+{
+	return VERSION;
+}
+
+bool WinnerStaysOn::exit(siz min, siz sec)
+{
+	log("exit()");
+	return true;
+}
+
+bool WinnerStaysOn::shutdown_game(siz min, siz sec)
+{
+	log("shutdown_game()");
+	return true;
+}
+
+bool WinnerStaysOn::warmup(siz min, siz sec)
+{
+	log("warmup()");
+	return true;
+}
+
+bool WinnerStaysOn::client_userinfo_changed(siz min, siz sec, siz num, siz team, const GUID& guid, const str& name)
+{
+	log("client_userinfo_changed(" << num << ", " << team << ", " << guid << ", " << name << ")");
+	log("clients[" << num << "]         : " << clients[num]);
+	log("players[clients[" << num << "]]: " << players[clients[num]]);
+	return true;
+}
+bool WinnerStaysOn::client_connect(siz min, siz sec, siz num)
+{
+	log("client_connect(" << num << ")");
+	return true;
+}
+bool WinnerStaysOn::client_disconnect(siz min, siz sec, siz num)
+{
+	log("client_disconnect(" << num << ")");
+	return true;
+}
+bool WinnerStaysOn::kill(siz min, siz sec, siz num1, siz num2, siz weap)
+{
+	log("kill(" << num1 << ", " << num2 << ", " << weap << ")");
+	return true;
+}
+bool WinnerStaysOn::ctf(siz min, siz sec, siz num, siz team, siz act)
+{
+	log("ctf(" << num << ", " << team << ", " << act<< ")");
+	return true;
+}
+bool WinnerStaysOn::award(siz min, siz sec, siz num, siz awd)
+{
+	log("award(" << num << ", " << awd << ")");
+	return true;
+}
+
+bool WinnerStaysOn::init_game(siz min, siz sec)
+{
+	log("init_game()");
+	log("mapname: " << mapname);
+	return true;
+}
+
+bool WinnerStaysOn::say(siz min, siz sec, const GUID& guid, const str& text)
+{
+	log("say(" << guid << ", " << text << ")");
+	return true;
+}
+
+bool WinnerStaysOn::unknown(siz min, siz sec, const str& cmd, const str& params)
+{
+	//log("unknown(" << line << ")");
+	return true;
+}
+
+void WinnerStaysOn::close()
+{
+}
+
+}} // katina::plugin
---
+++ src/plugins/WinnerStaysOn.h
@@ -0,0 +1,74 @@
+#pragma once
+#ifndef _OASTATS_KATINA_PLUGIN_WINNER_STAYS_ON_H
+#define	_OASTATS_KATINA_PLUGIN_WINNER_STAYS_ON_H
+/*
+ * File:   WinnerStaysOn.h
+ * Author: SooKee oasookee@gmail.com
+ *
+ * Created on May 06, 2013
+ */
+
+#include <map>
+#include <utility>
+
+#include <katina/Katina.h>
+#include <katina/KatinaPlugin.h>
+
+#include <katina/GUID.h>
+
+#include <katina/types.h>
+#include <katina/log.h>
+
+namespace katina { namespace plugin {
+
+using namespace oastats;
+using namespace oastats::log;
+using namespace oastats::types;
+
+class WinnerStaysOn
+: public KatinaPlugin
+{
+private:
+	str& mapname;
+	siz_guid_map& clients; // slot -> GUID
+	guid_str_map& players; // GUID -> name
+	guid_siz_map& teams; // GUID -> 'R' | 'B'
+
+public:
+	WinnerStaysOn(Katina& katina)
+	: KatinaPlugin(katina)
+	, mapname(katina.mapname)
+	, clients(katina.clients)
+	, players(katina.players)
+	, teams(katina.teams)
+	{
+	}
+
+	// INTERFACE: KatinaPlugin
+
+	virtual bool open();
+
+	virtual str get_id() const;
+	virtual str get_name() const;
+	virtual str get_version() const;
+
+	virtual bool exit(siz min, siz sec);
+	virtual bool shutdown_game(siz min, siz sec);
+	virtual bool warmup(siz min, siz sec);
+	virtual bool client_userinfo_changed(siz min, siz sec, siz num, siz team, const GUID& guid, const str& name);
+	virtual bool client_connect(siz min, siz sec, siz num);
+	virtual bool client_disconnect(siz min, siz sec, siz num);
+	virtual bool kill(siz min, siz sec, siz num1, siz num2, siz weap);
+	virtual bool ctf(siz min, siz sec, siz num, siz team, siz act);
+	virtual bool award(siz min, siz sec, siz num, siz awd);
+	virtual bool init_game(siz min, siz sec);
+	virtual bool say(siz min, siz sec, const GUID& guid, const str& text);
+	virtual bool unknown(siz min, siz sec, const str& cmd, const str& params);
+
+	virtual void close();
+};
+
+}} // katina::plugin
+
+#endif // _OASTATS_KATINA_PLUGIN_WINNER_STAYS_ON_H
+
