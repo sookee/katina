e7b4508fad6f25b910edee00f741f27b66dc1e86
SooKee <oasookee@gmail.com>
2014-04-21 22:59:00 +0100

    Fix: sql bug in player-db

--- src/plugins/KatinaPluginPlayerDb.cpp
+++ src/plugins/KatinaPluginPlayerDb.cpp
@@ -101,7 +101,7 @@ void db_add(const player_set::value_type& p)
 	bug("PLAYER DB: add: " << p.guid << " " << p.ip << " " << p.name);
 	std::ostringstream sql;
 
-	sql << "insert into `" << base << "`,`info` values (";
+	sql << "insert into `" << base << "`.`info` values (";
 	sql << p.guid << "," << p.ip << "," << p.name << ")";
 
 	insert(sql.str());
